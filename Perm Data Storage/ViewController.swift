25ed9b7a2f1ef51ea24b6da1f569eff66ea3f6cd
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-10 18:43:04 -0400

    Initial Commit

--- Perm Data Storage/ViewController.swift
+++ Perm Data Storage/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //NSUserDefaults.standardUserDefaults().setObject("Rosario", forKey: "name")
+        
+        var name = NSUserDefaults.standardUserDefaults().objectForKey("name")! as! String
+        
+        println(name)
+        
+        var arr = [1,2,3,4,5]
+        
+        //NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "array")
+        
+        var recalledArray = NSUserDefaults.standardUserDefaults().objectForKey("array")! as! NSArray
+        
+        println(recalledArray[2])
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
